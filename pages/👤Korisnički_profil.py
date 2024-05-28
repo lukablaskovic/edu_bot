fd85d2633b2d76f5ff407d621482862658748a93
lukablaskovic <lukablaskovic2000@gmail.com>
2024-05-28 22:54:08 +0200

    multi page streamlit

---
+++ pages/👤Korisnički_profil.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Korisnički profil",
+    page_icon="👤",
+)
+
+st.title('👤Korisnički profil')
+
+st.sidebar.markdown("# Korisnički profil")
+
